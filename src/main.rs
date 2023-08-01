e87552cb6b7cf36a154efdb1e2db6706231aee5b
Alan <alan@alanklas.com>
2023-08-01 13:12:43 +0200

    ok i need to start from the beninging, reading examples im starting to get it

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::f32::consts::PI;
 
-use bevy::{
-    pbr::CascadeShadowConfigBuilder,
-    prelude::*,
-    render::{mesh::Indices, render_resource::PrimitiveTopology},
-};
-use bevy_flycam::prelude::*;
+use bevy::prelude::*;
+use bevy::render::mesh::Indices;
+use bevy::render::mesh::VertexAttributeValues;
+use bevy::render::render_resource::PrimitiveTopology;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use noise::{NoiseFn, Perlin};
 
-const SEED: u32 = 1;
-const CHUNK_SIZE: u32 = 32;
+const SEED: u32 = 2137;
+const CHUNK_SIZE: u8 = 16;
+
+// Define a "marker" component to mark the custom mesh. Marker components are often used in Bevy for
+// filtering entities in queries with With, they're usually not queried directly since they don't contain information within them.
+#[derive(Component)]
+struct CustomUV;
 
 #[derive(Component)]
 struct Chunk;
@@ -24,204 +27,237 @@ enum BlockType {
 fn main() {
     App::new()
         .insert_resource(Msaa::Sample4)
-        // .add_system(bevy::window::close_on_esc)
-        .add_plugins(DefaultPlugins)
-        .add_plugin(PlayerPlugin)
-        .add_plugin(WorldInspectorPlugin::new())
-        .insert_resource(MovementSettings {
-            sensitivity: 0.00015, // default: 0.00012
-            speed: 12.0,          // default: 12.0
-        })
-        .add_startup_system(setup)
-        .add_systems((chunk_handling,))
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(WorldInspectorPlugin::new())
+        // Systems
+        .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Update, input_handler)
         .run();
 }
 
-macro_rules! get_chunk_position {
-    ($camera_position:expr) => {{
-        let chunk_position = Vec3::new(
-            ($camera_position.x / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32,
-            ($camera_position.y / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32,
-            ($camera_position.z / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32,
-        );
-        chunk_position
-    }};
-}
+fn setup(
+    mut commands: Commands,
+    asset_server: ResMut<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    // Import the custom texture.
+    let custom_texture_handle: Handle<Image> = asset_server.load("textures/dirt.png");
+    // Create and save a handle to the mesh.
+    let cube_mesh_handle: Handle<Mesh> = meshes.add(create_cube_mesh());
 
-macro_rules! get_block_verticies {
-    ($position:expr) => {{
-        // Get the verticies of a block
-        vec![
-            [$position.x - 0.5, $position.y - 0.5, $position.z - 0.5],
-            [$position.x - 0.5, $position.y - 0.5, $position.z + 0.5],
-            [$position.x + 0.5, $position.y - 0.5, $position.z + 0.5],
-            [$position.x + 0.5, $position.y - 0.5, $position.z - 0.5],
-            [$position.x - 0.5, $position.y + 0.5, $position.z - 0.5],
-            [$position.x - 0.5, $position.y + 0.5, $position.z + 0.5],
-            [$position.x + 0.5, $position.y + 0.5, $position.z + 0.5],
-            [$position.x + 0.5, $position.y + 0.5, $position.z - 0.5],
-        ]
-    }};
-}
-
-/// set up a simple 3D scene
-fn setup(mut commands: Commands) {
-    // Directional Light
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            shadows_enabled: true,
+    // Render the mesh with the custom texture using a PbrBundle, add the marker.
+    commands.spawn((
+        PbrBundle {
+            mesh: cube_mesh_handle,
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(custom_texture_handle),
+                ..default()
+            }),
             ..default()
         },
-        transform: Transform {
-            translation: Vec3::new(0.0, 2.0, 0.0),
-            rotation: Quat::from_rotation_x(-PI / 4.),
-            ..default()
-        },
-        // The default cascade config is designed to handle large scenes.
-        // As this example has a much smaller world, we can tighten the shadow
-        // bounds for better visual quality.
-        cascade_shadow_config: CascadeShadowConfigBuilder {
-            first_cascade_far_bound: 4.0,
-            maximum_distance: 10.0,
-            ..default()
-        }
-        .into(),
+        CustomUV,
+    ));
+
+    // Transform for the camera and lighting, looking at (0,0,0) (the position of the mesh).
+    let camera_and_light_transform =
+        Transform::from_xyz(1.8, 1.8, 1.8).looking_at(Vec3::ZERO, Vec3::Y);
+
+    // Camera in 3D space.
+    commands.spawn(Camera3dBundle {
+        transform: camera_and_light_transform,
         ..default()
     });
 
-    // info!("Move camera around by using WASD for lateral movement");
-    // info!("Use Left Shift and Spacebar for vertical movement");
-    // info!("Use the mouse to look around");
-    // info!("Press Esc to hide or show the mouse cursor");
-}
-
-fn chunk_handling(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    camera: Query<&Transform, With<FlyCam>>,
-    chunks: Query<&Transform, With<Chunk>>,
-) {
-    // Get the camera position
-    let camera_position = camera.single().translation;
-
-    // Check if all chunks are loaded around the camera in a 8x8 chunk radius
-    for x in 0..8 {
-        for y in 0..8 {
-            for z in 0..8 {
-                // Get the position of the chunk to generate
-                let chunk_position = Vec3::new(
-                    (camera_position.x / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32
-                        + x as f32 * CHUNK_SIZE as f32,
-                    (camera_position.y / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32
-                        + y as f32 * CHUNK_SIZE as f32,
-                    (camera_position.z / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32
-                        + z as f32 * CHUNK_SIZE as f32,
-                );
-                // Check if the chunk is already loaded
-                let mut chunk_loaded = false;
-                for chunk in chunks.iter() {
-                    if chunk.translation == chunk_position {
-                        chunk_loaded = true;
-                    }
-                }
-                // If the chunk is not loaded, generate it
-                if !chunk_loaded {
-                    generate_chunk_mesh(&mut commands, &mut meshes, &mut materials, chunk_position);
-                }
-            }
-        }
-    }
-}
-
-fn generate_chunk_mesh(
-    commands: &mut Commands,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>,
-    chunk_position: Vec3,
-) {
-    // To generate a chunk, we need a list of verticies and a list of indicies
-    let mut verticies: Vec<[f32; 3]> = Vec::new();
-    // To generate a chunk we can use perlin noise
-    let perlin = Perlin::new(SEED);
-    // Generate a block for each position in the chunk
-    for x in 0..CHUNK_SIZE {
-        for y in 0..CHUNK_SIZE {
-            for z in 0..CHUNK_SIZE {
-                // Get the position of the block
-                let position = Vec3::new(x as f32, y as f32, z as f32);
-                // Get the block type
-                let block_type = get_block_type(&perlin, position);
-                // If the block is not air, add it to the list of verticies
-                if block_type != BlockType::Air {
-                    // Get the block verticies
-                    let block_verticies = get_block_verticies!(position);
-                    // Add the block verticies to the list of verticies
-                    for vertex in block_verticies.iter() {
-                        verticies.push(*vertex);
-                    }
-                }
-            }
-        }
-    }
-    // Remove duplicate verticies
-    remove_duplicate_verticies(&mut verticies);
-    // Create a mesh from the verticies
-    let mesh = create_mesh(&verticies);
-    // Create a chunk entity
-    commands.spawn((
-        Chunk,
-        PbrBundle {
-            mesh: meshes.add(mesh),
-            material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
-            transform: Transform::from_translation(chunk_position),
-            ..Default::default()
+    // Light up the scene.
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1000.0,
+            range: 100.0,
+            ..default()
         },
-    ));
-    info!("Chunk generated at {:?}", chunk_position);
+        transform: camera_and_light_transform,
+        ..default()
+    });
+
+    // Text to describe the controls.
+    commands.spawn(
+        TextBundle::from_section(
+            "Controls:\nSpace: Change UVs\nX/Y/Z: Rotate\nR: Reset orientation",
+            TextStyle {
+                font_size: 20.0,
+                ..default()
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        }),
+    );
 }
 
-fn get_block_type(perlin: &Perlin, position: Vec3) -> BlockType {
-    // Get the block type at a position
-    // TODO: Add more block types
-    let noise = perlin.get([position.x as f64, position.y as f64, position.z as f64]);
-    if noise > 0.0 {
-        BlockType::Dirt
-    } else {
-        BlockType::Air
+// System to receive input from the user,
+// check out examples/input/ for more examples about user input.
+fn input_handler(
+    keyboard_input: Res<Input<KeyCode>>,
+    mesh_query: Query<&Handle<Mesh>, With<CustomUV>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut query: Query<&mut Transform, With<CustomUV>>,
+    time: Res<Time>,
+) {
+    if keyboard_input.pressed(KeyCode::X) {
+        for mut transform in &mut query {
+            transform.rotate_x(time.delta_seconds() / 1.2);
+        }
     }
-}
-
-fn remove_duplicate_verticies(verticies: &mut Vec<[f32; 3]>) {
-    // If there are two or more verticies at the exact same position, remove until there is only one
-    for i in 0..verticies.len() {
-        for j in 0..verticies.len() {
-            if i != j && verticies[i] == verticies[j] {
-                verticies.remove(j);
-            }
+    if keyboard_input.pressed(KeyCode::Y) {
+        for mut transform in &mut query {
+            transform.rotate_y(time.delta_seconds() / 1.2);
+        }
+    }
+    if keyboard_input.pressed(KeyCode::Z) {
+        for mut transform in &mut query {
+            transform.rotate_z(time.delta_seconds() / 1.2);
+        }
+    }
+    if keyboard_input.pressed(KeyCode::R) {
+        for mut transform in &mut query {
+            transform.look_to(Vec3::NEG_Z, Vec3::Y);
         }
     }
 }
 
-// fn get_chunk_position(camera_position: Vec3) -> Vec3 {
-//     // Get the position of the chunk to generate
-//     let chunk_position = Vec3::new(
-//         (camera_position.x / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32,
-//         (camera_position.y / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32,
-//         (camera_position.z / CHUNK_SIZE as f32).floor() * CHUNK_SIZE as f32,
-//     );
-//     chunk_position
-// }
+fn create_cube_mesh() -> Mesh {
+    let mut cube_mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
-fn create_mesh(verticies: &Vec<[f32; 3]>) -> Mesh {
-    // Create a mesh from the verticies
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, verticies.to_vec());
-    // Generate indices
-    let mut indices: Vec<u32> = Vec::new();
-    for i in 0..verticies.len() {
-        indices.push(i as u32);
-    }
-    mesh.set_indices(Some(Indices::U32(indices)));
-    mesh
+    #[rustfmt::skip]
+    cube_mesh.insert_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        // Each array is an [x, y, z] coordinate in local space.
+        // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
+        // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
+        vec![
+            // top (facing towards +y)
+            [-0.5, 0.5, -0.5], // vertex with index 0
+            [0.5, 0.5, -0.5], // vertex with index 1
+            [0.5, 0.5, 0.5], // etc. until 23
+            [-0.5, 0.5, 0.5],
+            // bottom   (-y)
+            [-0.5, -0.5, -0.5],
+            [0.5, -0.5, -0.5],
+            [0.5, -0.5, 0.5],
+            [-0.5, -0.5, 0.5],
+            // right    (+x)
+            [0.5, -0.5, -0.5],
+            [0.5, -0.5, 0.5],
+            [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
+            [0.5, 0.5, -0.5],
+            // left     (-x)
+            [-0.5, -0.5, -0.5],
+            [-0.5, -0.5, 0.5],
+            [-0.5, 0.5, 0.5],
+            [-0.5, 0.5, -0.5],
+            // back     (+z)
+            [-0.5, -0.5, 0.5],
+            [-0.5, 0.5, 0.5],
+            [0.5, 0.5, 0.5],
+            [0.5, -0.5, 0.5],
+            // forward  (-z)
+            [-0.5, -0.5, -0.5],
+            [-0.5, 0.5, -0.5],
+            [0.5, 0.5, -0.5],
+            [0.5, -0.5, -0.5],
+        ],
+    );
+
+    // Set-up UV coordinates
+    // Note: (0.0, 0.0) = Top-Left in UV mapping, (1.0, 1.0) = Bottom-Right in UV mapping
+    #[rustfmt::skip]
+    cube_mesh.insert_attribute(
+        Mesh::ATTRIBUTE_UV_0,
+        vec![
+            // Assigning the UV coords for the top side.
+            [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0],
+            // Assigning the UV coords for the bottom side.
+            [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0],
+            // Assigning the UV coords for the right side.
+            [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0],
+            // Assigning the UV coords for the left side. 
+            [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0],
+            // Assigning the UV coords for the back side.
+            [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0],
+            // Assigning the UV coords for the forward side.
+            [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0],
+        ],
+    );
+
+    // For meshes with flat shading, normals are orthogonal (pointing out) from the direction of
+    // the surface.
+    // Normals are required for correct lighting calculations.
+    // Each array represents a normalized vector, which length should be equal to 1.0.
+    #[rustfmt::skip]
+    cube_mesh.insert_attribute(
+        Mesh::ATTRIBUTE_NORMAL,
+        vec![
+            // Normals for the top side (towards +y)
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            // Normals for the bottom side (towards -y)
+            [0.0, -1.0, 0.0],
+            [0.0, -1.0, 0.0],
+            [0.0, -1.0, 0.0],
+            [0.0, -1.0, 0.0],
+            // Normals for the right side (towards +x)
+            [1.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            // Normals for the left side (towards -x)
+            [-1.0, 0.0, 0.0],
+            [-1.0, 0.0, 0.0],
+            [-1.0, 0.0, 0.0],
+            [-1.0, 0.0, 0.0],
+            // Normals for the back side (towards +z)
+            [0.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0],
+            // Normals for the forward side (towards -z)
+            [0.0, 0.0, -1.0],
+            [0.0, 0.0, -1.0],
+            [0.0, 0.0, -1.0],
+            [0.0, 0.0, -1.0],
+        ],
+    );
+
+    // Create the triangles out of the 24 vertices we created.
+    // To construct a square, we need 2 triangles, therefore 12 triangles in total.
+    // To construct a triangle, we need the indices of its 3 defined vertices, adding them one
+    // by one, in a counter-clockwise order (relative to the position of the viewer, the order
+    // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
+    // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
+    // further examples and the implementation of the built-in shapes.
+    #[rustfmt::skip]
+    cube_mesh.set_indices(Some(Indices::U32(vec![
+        0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
+        4,5,7 , 5,6,7, // bottom (-y) 
+        8,11,9 , 9,11,10, // right (+x)
+        12,13,15 , 13,14,15, // left (-x)
+        16,19,17 , 17,19,18, // back (+z)
+        20,21,23 , 21,22,23, // forward (-z)
+    ])));
+
+    cube_mesh
+}
+
+/// Creates a 32x32x32 chunk mesh using 3D Perlin noise.
+/// The mesh is created by sampling the noise function at each vertex position.
+/// If the noise value is above a certain threshold, a cube is created at that position.
+fn create_chunk_mesh() -> Mesh {
+    todo!();
 }
