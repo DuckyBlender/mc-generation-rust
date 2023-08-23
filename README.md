97bf758a31cb2cc4854ed4be5c1f468237131ff6
Alan <alan@alanklas.com>
2023-08-23 22:03:01 +0200

    rewriting once again xd

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This is a voxel generation example in Rust using the Bevy game engine.
 
+## Branch
+
+This branch is a WIP rewrite. I decided to re-write this to implement entities & a server. Also it include much more optimizations like using a 1d array and 3d chunks. I will also try to make it more modular and easier to understand.
+
 ## Running
 
 To run this example, you need to have Rust installed. You can install Rust [here](https://www.rust-lang.org/tools/install).
@@ -11,7 +15,7 @@ Once you have Rust installed, you can run the example with the following command
 ```bash
 cargo run --release
 ```
-# WARNING! RIGHT NOW THE GAME RANDOMLY CRASHES. NO IDEA WHY. I'LL FIX IT SOON. (I HOPE) (I'M NOT SURE IF IT'S A BEVY BUG OR MY CODE) (I THINK IT'S MY CODE) 
+
 ## Controls
 
 - `WASD` - Move
@@ -23,11 +27,11 @@ cargo run --release
 
 - [x] Implement a crude cube spawning system thingy
 - [x] Optimize it using a chunk system and meshing
-- [x] Fix chunk borders (don't render the faces that are touching other chunks but connect them seamlessly)
-- [x] Add more textures (grass, dirt, stone, etc.) (correct orientation)
-- [x] Add a skybox
-- [x] Add a surface generator
-- [x] Multithreading (chunk generation, meshing, etc.)
+- [ ] Fix chunk borders (don't render the faces that are touching other chunks but connect them seamlessly)
+- [ ] Add more textures (grass, dirt, stone, etc.) (correct orientation)
+- [ ] Add a skybox
+- [ ] Add a surface generator
+- [ ] Multithreading (chunk generation, meshing, etc.)
 - [ ] Add a player controller (first person) [TOP PRIORITY]
 - [ ] Blend the surface generator nicely
 - [ ] Add block breaking and placing
@@ -35,4 +39,4 @@ cargo run --release
 
 ## License
 
-You can do whatever you want with this code. I don't care. Just don't sue me if it breaks something.
+You can do whatever you want with this code. I don't care. I hope it helps you in some way.
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use bevy::{
         render_resource::PrimitiveTopology,
     },
 };
+use bevy_flycam::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use noise::{NoiseFn, Perlin};
 
@@ -12,6 +13,8 @@ const CHUNK_HEIGHT: usize = 32;
 const CHUNK_WIDTH: usize = 32;
 const CHUNK_DEPTH: usize = 32;
 const CHUNK_SIZE: usize = CHUNK_HEIGHT * CHUNK_WIDTH * CHUNK_DEPTH;
+
+const WORLD_SCALE: f64 = 0.05;
 
 // converting from 1d to 3d array
 macro_rules! to_3d {
@@ -37,25 +40,34 @@ macro_rules! to_1d {
 enum BlockType {
     #[default]
     Air = 0,
-    Grass = 1,
-    Dirt = 2,
-    Stone = 3,
+    Stone = 1,
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(WorldInspectorPlugin::new())
-        .add_systems(Startup, (setup, create_chunk))
+        .add_plugins(PlayerPlugin)
+        .insert_resource(MovementSettings {
+            sensitivity: 0.00015, // default: 0.00012
+            speed: 50.0,          // default: 12.0
+        })
+        .insert_resource(KeyBindings {
+            move_ascend: KeyCode::Space,
+            move_descend: KeyCode::ShiftLeft,
+            ..Default::default()
+        })
+        .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    // Spawn camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut ambient_light: ResMut<AmbientLight>,
+) {
+    // Camera is spawned by plugin
 
     // Spawn sun
     commands.spawn(DirectionalLightBundle {
@@ -69,16 +81,28 @@ fn setup(mut commands: Commands) {
     });
 
     // Ambient light
-    commands.insert_resource(AmbientLight {
-        color: Color::GRAY * 0.2,
-        ..default()
-    });
+    ambient_light.color = Color::GRAY;
+
+    // Spawn chunks
+    for x in -10..10 {
+        for y in -2..2 {
+            for z in -10..10 {
+                create_and_spawn_chunk(
+                    &mut commands,
+                    &mut meshes,
+                    &mut materials,
+                    IVec3::new(x, y, z),
+                );
+            }
+        }
+    }
 }
 
-fn create_chunk(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+fn create_and_spawn_chunk(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    chunk_position: IVec3,
 ) {
     let now = std::time::Instant::now();
     // Create a new mesh.
@@ -91,21 +115,47 @@ fn create_chunk(
     let perlin = Perlin::new(2137);
     // 1d array for performance
     let mut chunk: [BlockType; CHUNK_SIZE] = [BlockType::Air; CHUNK_SIZE];
-    for x in 0..CHUNK_WIDTH {
-        for y in 0..CHUNK_HEIGHT {
-            for z in 0..CHUNK_DEPTH {
-                let noise = perlin.get([x as f64 / 10.0, y as f64 / 10.0, z as f64 / 10.0]) as f32;
+    for x in 0..CHUNK_WIDTH as i32 {
+        for y in 0..CHUNK_HEIGHT as i32 {
+            for z in 0..CHUNK_DEPTH as i32 {
+                // "Globalize" the coordinates for perlin noise
+                let x = x + chunk_position.x * CHUNK_WIDTH as i32;
+                let y = y + chunk_position.y * CHUNK_HEIGHT as i32;
+                let z = z + chunk_position.z * CHUNK_DEPTH as i32;
+
+                let noise = perlin.get([
+                    x as f64 * WORLD_SCALE,
+                    y as f64 * WORLD_SCALE,
+                    z as f64 * WORLD_SCALE,
+                ]);
+
                 let block = if noise > 0.0 {
-                    BlockType::Grass
-                } else if noise > -0.1 {
-                    BlockType::Dirt
-                } else {
                     BlockType::Stone
+                } else {
+                    BlockType::Air
                 };
-                chunk[to_1d!(x, y, z)] = block;
+
+                // "Un-globalize" the coordinates for the chunk data
+                let x = x - chunk_position.x * CHUNK_WIDTH as i32;
+                let y = y - chunk_position.y * CHUNK_HEIGHT as i32;
+                let z = z - chunk_position.z * CHUNK_DEPTH as i32;
+
+                chunk[to_1d!(x as usize, y as usize, z as usize)] = block;
             }
         }
     }
+
+    // Count amount of air, dirt and stone blocks
+    let mut air = 0;
+
+    let mut stone = 0;
+    for block in &chunk {
+        match block {
+            BlockType::Air => air += 1,
+            BlockType::Stone => stone += 1,
+        }
+    }
+    info!("Air: {}, Stone: {}, Total: {}", air, stone, CHUNK_SIZE);
 
     // Chunk finished generating, now create verticies and indices
     for i in 0..CHUNK_SIZE {
@@ -114,19 +164,21 @@ fn create_chunk(
             continue;
         }
         let [x, y, z] = to_3d!(i);
-        // top
+
+        // top (+y)
         if y == CHUNK_HEIGHT - 1 || chunk[to_1d!(x, y + 1, z)] == BlockType::Air {
-            vertices.push([x as f32, y as f32 + 1.0, z as f32]);
-            vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32]);
-            vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32 + 1.0]);
             vertices.push([x as f32, y as f32 + 1.0, z as f32 + 1.0]);
+            vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32 + 1.0]);
+            vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32]);
+            vertices.push([x as f32, y as f32 + 1.0, z as f32]);
+
             // add normals
             normals.push([0.0, 1.0, 0.0]);
             normals.push([0.0, 1.0, 0.0]);
             normals.push([0.0, 1.0, 0.0]);
             normals.push([0.0, 1.0, 0.0]);
         }
-        // bottom
+        // bottom (-y)
         if y == 0 || chunk[to_1d!(x, y - 1, z)] == BlockType::Air {
             vertices.push([x as f32, y as f32, z as f32]);
             vertices.push([x as f32 + 1.0, y as f32, z as f32]);
@@ -138,19 +190,20 @@ fn create_chunk(
             normals.push([0.0, -1.0, 0.0]);
             normals.push([0.0, -1.0, 0.0]);
         }
-        // left
+        // left (-x)
         if x == 0 || chunk[to_1d!(x - 1, y, z)] == BlockType::Air {
-            vertices.push([x as f32, y as f32, z as f32]);
-            vertices.push([x as f32, y as f32 + 1.0, z as f32]);
-            vertices.push([x as f32, y as f32 + 1.0, z as f32 + 1.0]);
             vertices.push([x as f32, y as f32, z as f32 + 1.0]);
+            vertices.push([x as f32, y as f32 + 1.0, z as f32 + 1.0]);
+            vertices.push([x as f32, y as f32 + 1.0, z as f32]);
+            vertices.push([x as f32, y as f32, z as f32]);
+
             // add normals
             normals.push([-1.0, 0.0, 0.0]);
             normals.push([-1.0, 0.0, 0.0]);
             normals.push([-1.0, 0.0, 0.0]);
             normals.push([-1.0, 0.0, 0.0]);
         }
-        // right
+        // right (+x)
         if x == CHUNK_WIDTH - 1 || chunk[to_1d!(x + 1, y, z)] == BlockType::Air {
             vertices.push([x as f32 + 1.0, y as f32, z as f32]);
             vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32]);
@@ -162,19 +215,20 @@ fn create_chunk(
             normals.push([1.0, 0.0, 0.0]);
             normals.push([1.0, 0.0, 0.0]);
         }
-        // front
+        // front (-z)
         if z == 0 || chunk[to_1d!(x, y, z - 1)] == BlockType::Air {
-            vertices.push([x as f32, y as f32, z as f32]);
-            vertices.push([x as f32 + 1.0, y as f32, z as f32]);
-            vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32]);
             vertices.push([x as f32, y as f32 + 1.0, z as f32]);
+            vertices.push([x as f32 + 1.0, y as f32 + 1.0, z as f32]);
+            vertices.push([x as f32 + 1.0, y as f32, z as f32]);
+            vertices.push([x as f32, y as f32, z as f32]);
+
             // add normal
             normals.push([0.0, 0.0, -1.0]);
             normals.push([0.0, 0.0, -1.0]);
             normals.push([0.0, 0.0, -1.0]);
             normals.push([0.0, 0.0, -1.0]);
         }
-        // back
+        // back (+z)
         if z == CHUNK_DEPTH - 1 || chunk[to_1d!(x, y, z + 1)] == BlockType::Air {
             vertices.push([x as f32, y as f32, z as f32 + 1.0]);
             vertices.push([x as f32 + 1.0, y as f32, z as f32 + 1.0]);
@@ -186,6 +240,13 @@ fn create_chunk(
             normals.push([0.0, 0.0, 1.0]);
             normals.push([0.0, 0.0, 1.0]);
         }
+    }
+
+    // Move all vertices to the correct position
+    for vertex in &mut vertices {
+        vertex[0] += chunk_position.x as f32 * CHUNK_WIDTH as f32;
+        vertex[1] += chunk_position.y as f32 * CHUNK_HEIGHT as f32;
+        vertex[2] += chunk_position.z as f32 * CHUNK_DEPTH as f32;
     }
 
     // Create indices
@@ -226,7 +287,7 @@ fn create_chunk(
     // Spawn the chunk
     commands.spawn(PbrBundle {
         mesh: chunk_mesh_handle,
-        material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+        material: materials.add(Color::rgb(0.5, 1.0, 0.0).into()),
         ..Default::default()
     });
 
